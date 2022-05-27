3a5627e451dd6551bf4fd698c62ec815bb9aa886
KevinVeliz <mario-veliz258934@hotmail.com>
2022-05-26 22:55:44 -0500

    Exercise 25-05

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'app-moviles-efcb1',
+    appId: '1:770578917328:web:97e5d4a7c2d031fd973bd5',
+    storageBucket: 'app-moviles-efcb1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDaMtUzAJn7vLy0pktOvKRIitS9wJ1Kuxg',
+    authDomain: 'app-moviles-efcb1.firebaseapp.com',
+    messagingSenderId: '770578917328',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'app-moviles-efcb1',
+    appId: '1:770578917328:web:97e5d4a7c2d031fd973bd5',
+    storageBucket: 'app-moviles-efcb1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDaMtUzAJn7vLy0pktOvKRIitS9wJ1Kuxg',
+    authDomain: 'app-moviles-efcb1.firebaseapp.com',
+    messagingSenderId: '770578917328',
+  },
   production: false
 };
 
